b7d58a43ba32d6a684a1a1bb72f626385d7ccfdc
Lucca Ambrosius <ismartlucca@gmail.com>
2023-03-01 19:34:53 -0300

    global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -10,9 +10,17 @@ export const {
   theme,
   createTheme,
 } = createStitches({
-  theme: {
-    colors: {
-      rocketseat: '#8257e6',
+    theme: {
+      colors: {
+        white: '#FFF',
+  
+        gray900: '#121214',
+        gray800: '#202024',
+        gray300: '#c4c4cc',
+        gray100: '#e1e1e6',
+  
+        green500: '#00875f',
+        green300: '#00b37e',
+      }
     }
-  }
 })
