001bc84b2195a42a346074c2eb144819a3c708a9
Lucca Ambrosius <ismartlucca@gmail.com>
2023-03-05 19:09:40 -0300

    SSG with dinamic param

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,8 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
-
 interface ProductProps {
   product: {
     id: string
@@ -13,20 +12,16 @@ interface ProductProps {
     description: string
   }
 }
-
 export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
       <ImageContainer>
         <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
-
       <ProductDetails>
         <h1>{product.name}</h1>
         <span>{product.price}</span>
-
         <p>{product.description}</p>
-
         <button>
           Comprar agora
         </button>
@@ -35,15 +30,24 @@ export default function Product({ product }: ProductProps) {
   )
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: 'prod_NTJtKyo7qMVhhb' }
+      }
+    ],
+    fallback: false,
+  }
+}
+
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
   const productId = params.id;
 
   const product = await stripe.products.retrieve(productId, {
     expand: ['default_price']
   });
-
   const price = product.default_price as Stripe.Price;
-
   return {
     props: {
       product: {
